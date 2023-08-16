1dca0fded90b8e1d5f4cefc4f277debdd079dba2
Frederik Wagner <72168283+Freddynator8@users.noreply.github.com>
2023-08-16 13:13:51 +0200

    Fixing enum problem

--- sc-frontend/src/App.js
+++ sc-frontend/src/App.js
@@ -1,28 +1,28 @@
 import {useState} from "react";
 
+const Turn = {
+    white: 0,
+    black: 1
+}
+const FieldTypes = {
+    default: 0,
+    dot: 1,
+    knightBlack: 2,
+    knightWhite: 3,
+    knightBlackAttacked: 4,
+    knightWhiteAttacked: 5,
+    queenBlack: 6,
+    queenWhite: 7,
+    queenBlackAttacked: 8,
+    queenWhiteAttacked: 9,
+}
+
 function Square({value, onSquareClicked}){
   return(
       <button className="square" onClick={onSquareClicked}>
         {value}
       </button>
   );
-}
-
-export enum FieldTypes {
-    default,
-    dot,
-    knightBlack,
-    knightWhite,
-    knightBlackAttacked,
-    knightWhiteAttacked,
-    queenBlack,
-    queenWhite,
-    queenBlackAttacked,
-    queenWhiteAttacked,
-}
-export enum Turn {
-    white,
-    black,
 }
 
 function Board({squares,isSelected}){
@@ -209,87 +209,111 @@ function Board({squares,isSelected}){
             }
         }
     }
+    function parseCharacter(square){
+        switch (square){
+            case FieldTypes.dot:
+                return "X";
+            case FieldTypes.knightBlack:
+                return "KB";
+            case FieldTypes.knightWhite:
+                return "KW";
+            case FieldTypes.knightBlackAttacked:
+                return "KBA";
+            case FieldTypes.knightWhiteAttacked:
+                return "KWA";
+            case FieldTypes.queenBlack:
+                return "QB";
+            case FieldTypes.queenWhite:
+                return "QW";
+            case FieldTypes.queenBlackAttacked:
+                return "QBA";
+            case FieldTypes.queenWhiteAttacked:
+                return "QWA";
+            default:
+                return "";
+        }
+    }
   return(
       <>
           <div className="board-row">
-              <Square value={squares[0]} onSquareClicked={() => handleSquareClicked(0)}/>
-              <Square value={squares[1]} onSquareClicked={() => handleSquareClicked(1)}/>
-              <Square value={squares[2]} onSquareClicked={() => handleSquareClicked(2)}/>
-              <Square value={squares[3]} onSquareClicked={() => handleSquareClicked(3)}/>
-              <Square value={squares[4]} onSquareClicked={() => handleSquareClicked(4)}/>
-              <Square value={squares[5]} onSquareClicked={() => handleSquareClicked(5)}/>
-              <Square value={squares[6]} onSquareClicked={() => handleSquareClicked(6)}/>
-              <Square value={squares[7]} onSquareClicked={() => handleSquareClicked(7)}/>
+              <Square value={parseCharacter(squares[0])} onSquareClicked={() => handleSquareClicked(0)}/>
+              <Square value={parseCharacter(squares[1])} onSquareClicked={() => handleSquareClicked(1)}/>
+              <Square value={parseCharacter(squares[2])} onSquareClicked={() => handleSquareClicked(2)}/>
+              <Square value={parseCharacter(squares[3])} onSquareClicked={() => handleSquareClicked(3)}/>
+              <Square value={parseCharacter(squares[4])} onSquareClicked={() => handleSquareClicked(4)}/>
+              <Square value={parseCharacter(squares[5])} onSquareClicked={() => handleSquareClicked(5)}/>
+              <Square value={parseCharacter(squares[6])} onSquareClicked={() => handleSquareClicked(6)}/>
+              <Square value={parseCharacter(squares[7])} onSquareClicked={() => handleSquareClicked(7)}/>
           </div>
           <div className="board-row">
-              <Square value={squares[8]} onSquareClicked={() => handleSquareClicked(8)}/>
-              <Square value={squares[9]} onSquareClicked={() => handleSquareClicked(9)}/>
-              <Square value={squares[10]} onSquareClicked={() => handleSquareClicked(10)}/>
-              <Square value={squares[11]} onSquareClicked={() => handleSquareClicked(11)}/>
-              <Square value={squares[12]} onSquareClicked={() => handleSquareClicked(12)}/>
-              <Square value={squares[13]} onSquareClicked={() => handleSquareClicked(13)}/>
-              <Square value={squares[14]} onSquareClicked={() => handleSquareClicked(14)}/>
-              <Square value={squares[15]} onSquareClicked={() => handleSquareClicked(15)}/>
+              <Square value={parseCharacter(squares[8])} onSquareClicked={() => handleSquareClicked(8)}/>
+              <Square value={parseCharacter(squares[9])} onSquareClicked={() => handleSquareClicked(9)}/>
+              <Square value={parseCharacter(squares[10])} onSquareClicked={() => handleSquareClicked(10)}/>
+              <Square value={parseCharacter(squares[11])} onSquareClicked={() => handleSquareClicked(11)}/>
+              <Square value={parseCharacter(squares[12])} onSquareClicked={() => handleSquareClicked(12)}/>
+              <Square value={parseCharacter(squares[13])} onSquareClicked={() => handleSquareClicked(13)}/>
+              <Square value={parseCharacter(squares[14])} onSquareClicked={() => handleSquareClicked(14)}/>
+              <Square value={parseCharacter(squares[15])} onSquareClicked={() => handleSquareClicked(15)}/>
           </div>
           <div className="board-row">
-              <Square value={squares[16]} onSquareClicked={() => handleSquareClicked(16)}/>
-              <Square value={squares[17]} onSquareClicked={() => handleSquareClicked(17)}/>
-              <Square value={squares[18]} onSquareClicked={() => handleSquareClicked(18)}/>
-              <Square value={squares[19]} onSquareClicked={() => handleSquareClicked(19)}/>
-              <Square value={squares[20]} onSquareClicked={() => handleSquareClicked(20)}/>
-              <Square value={squares[21]} onSquareClicked={() => handleSquareClicked(21)}/>
-              <Square value={squares[22]} onSquareClicked={() => handleSquareClicked(22)}/>
-              <Square value={squares[23]} onSquareClicked={() => handleSquareClicked(23)}/>
+              <Square value={parseCharacter(squares[16])} onSquareClicked={() => handleSquareClicked(16)}/>
+              <Square value={parseCharacter(squares[17])} onSquareClicked={() => handleSquareClicked(17)}/>
+              <Square value={parseCharacter(squares[18])} onSquareClicked={() => handleSquareClicked(18)}/>
+              <Square value={parseCharacter(squares[19])} onSquareClicked={() => handleSquareClicked(19)}/>
+              <Square value={parseCharacter(squares[20])} onSquareClicked={() => handleSquareClicked(20)}/>
+              <Square value={parseCharacter(squares[21])} onSquareClicked={() => handleSquareClicked(21)}/>
+              <Square value={parseCharacter(squares[22])} onSquareClicked={() => handleSquareClicked(22)}/>
+              <Square value={parseCharacter(squares[23])} onSquareClicked={() => handleSquareClicked(23)}/>
           </div>
           <div className="board-row">
-              <Square value={squares[24]} onSquareClicked={() => handleSquareClicked(24)}/>
-              <Square value={squares[25]} onSquareClicked={() => handleSquareClicked(25)}/>
-              <Square value={squares[26]} onSquareClicked={() => handleSquareClicked(26)}/>
-              <Square value={squares[27]} onSquareClicked={() => handleSquareClicked(27)}/>
-              <Square value={squares[28]} onSquareClicked={() => handleSquareClicked(28)}/>
-              <Square value={squares[29]} onSquareClicked={() => handleSquareClicked(29)}/>
-              <Square value={squares[30]} onSquareClicked={() => handleSquareClicked(30)}/>
-              <Square value={squares[31]} onSquareClicked={() => handleSquareClicked(31)}/>
+              <Square value={parseCharacter(squares[24])} onSquareClicked={() => handleSquareClicked(24)}/>
+              <Square value={parseCharacter(squares[25])} onSquareClicked={() => handleSquareClicked(25)}/>
+              <Square value={parseCharacter(squares[26])} onSquareClicked={() => handleSquareClicked(26)}/>
+              <Square value={parseCharacter(squares[27])} onSquareClicked={() => handleSquareClicked(27)}/>
+              <Square value={parseCharacter(squares[28])} onSquareClicked={() => handleSquareClicked(28)}/>
+              <Square value={parseCharacter(squares[29])} onSquareClicked={() => handleSquareClicked(29)}/>
+              <Square value={parseCharacter(squares[30])} onSquareClicked={() => handleSquareClicked(30)}/>
+              <Square value={parseCharacter(squares[31])} onSquareClicked={() => handleSquareClicked(31)}/>
           </div>
           <div className="board-row">
-              <Square value={squares[32]} onSquareClicked={() => handleSquareClicked(32)}/>
-              <Square value={squares[33]} onSquareClicked={() => handleSquareClicked(33)}/>
-              <Square value={squares[34]} onSquareClicked={() => handleSquareClicked(34)}/>
-              <Square value={squares[35]} onSquareClicked={() => handleSquareClicked(35)}/>
-              <Square value={squares[36]} onSquareClicked={() => handleSquareClicked(36)}/>
-              <Square value={squares[37]} onSquareClicked={() => handleSquareClicked(37)}/>
-              <Square value={squares[38]} onSquareClicked={() => handleSquareClicked(38)}/>
-              <Square value={squares[39]} onSquareClicked={() => handleSquareClicked(39)}/>
+              <Square value={parseCharacter(squares[32])} onSquareClicked={() => handleSquareClicked(32)}/>
+              <Square value={parseCharacter(squares[33])} onSquareClicked={() => handleSquareClicked(33)}/>
+              <Square value={parseCharacter(squares[34])} onSquareClicked={() => handleSquareClicked(34)}/>
+              <Square value={parseCharacter(squares[35])} onSquareClicked={() => handleSquareClicked(35)}/>
+              <Square value={parseCharacter(squares[36])} onSquareClicked={() => handleSquareClicked(36)}/>
+              <Square value={parseCharacter(squares[37])} onSquareClicked={() => handleSquareClicked(37)}/>
+              <Square value={parseCharacter(squares[38])} onSquareClicked={() => handleSquareClicked(38)}/>
+              <Square value={parseCharacter(squares[39])} onSquareClicked={() => handleSquareClicked(39)}/>
           </div>
           <div className="board-row">
-              <Square value={squares[40]} onSquareClicked={() => handleSquareClicked(40)}/>
-              <Square value={squares[41]} onSquareClicked={() => handleSquareClicked(41)}/>
-              <Square value={squares[42]} onSquareClicked={() => handleSquareClicked(42)}/>
-              <Square value={squares[43]} onSquareClicked={() => handleSquareClicked(43)}/>
-              <Square value={squares[44]} onSquareClicked={() => handleSquareClicked(44)}/>
-              <Square value={squares[45]} onSquareClicked={() => handleSquareClicked(45)}/>
-              <Square value={squares[46]} onSquareClicked={() => handleSquareClicked(46)}/>
-              <Square value={squares[47]} onSquareClicked={() => handleSquareClicked(47)}/>
+              <Square value={parseCharacter(squares[40])} onSquareClicked={() => handleSquareClicked(40)}/>
+              <Square value={parseCharacter(squares[41])} onSquareClicked={() => handleSquareClicked(41)}/>
+              <Square value={parseCharacter(squares[42])} onSquareClicked={() => handleSquareClicked(42)}/>
+              <Square value={parseCharacter(squares[43])} onSquareClicked={() => handleSquareClicked(43)}/>
+              <Square value={parseCharacter(squares[44])} onSquareClicked={() => handleSquareClicked(44)}/>
+              <Square value={parseCharacter(squares[45])} onSquareClicked={() => handleSquareClicked(45)}/>
+              <Square value={parseCharacter(squares[46])} onSquareClicked={() => handleSquareClicked(46)}/>
+              <Square value={parseCharacter(squares[47])} onSquareClicked={() => handleSquareClicked(47)}/>
           </div>
           <div className="board-row">
-              <Square value={squares[48]} onSquareClicked={() => handleSquareClicked(48)}/>
-              <Square value={squares[49]} onSquareClicked={() => handleSquareClicked(49)}/>
-              <Square value={squares[50]} onSquareClicked={() => handleSquareClicked(50)}/>
-              <Square value={squares[51]} onSquareClicked={() => handleSquareClicked(51)}/>
-              <Square value={squares[52]} onSquareClicked={() => handleSquareClicked(52)}/>
-              <Square value={squares[53]} onSquareClicked={() => handleSquareClicked(53)}/>
-              <Square value={squares[54]} onSquareClicked={() => handleSquareClicked(54)}/>
-              <Square value={squares[55]} onSquareClicked={() => handleSquareClicked(55)}/>
+              <Square value={parseCharacter(squares[48])} onSquareClicked={() => handleSquareClicked(48)}/>
+              <Square value={parseCharacter(squares[49])} onSquareClicked={() => handleSquareClicked(49)}/>
+              <Square value={parseCharacter(squares[50])} onSquareClicked={() => handleSquareClicked(50)}/>
+              <Square value={parseCharacter(squares[51])} onSquareClicked={() => handleSquareClicked(51)}/>
+              <Square value={parseCharacter(squares[52])} onSquareClicked={() => handleSquareClicked(52)}/>
+              <Square value={parseCharacter(squares[53])} onSquareClicked={() => handleSquareClicked(53)}/>
+              <Square value={parseCharacter(squares[54])} onSquareClicked={() => handleSquareClicked(54)}/>
+              <Square value={parseCharacter(squares[55])} onSquareClicked={() => handleSquareClicked(55)}/>
           </div>
           <div className="board-row">
-              <Square value={squares[56]} onSquareClicked={() => handleSquareClicked(56)}/>
-              <Square value={squares[57]} onSquareClicked={() => handleSquareClicked(57)}/>
-              <Square value={squares[58]} onSquareClicked={() => handleSquareClicked(58)}/>
-              <Square value={squares[59]} onSquareClicked={() => handleSquareClicked(59)}/>
-              <Square value={squares[60]} onSquareClicked={() => handleSquareClicked(60)}/>
-              <Square value={squares[61]} onSquareClicked={() => handleSquareClicked(61)}/>
-              <Square value={squares[62]} onSquareClicked={() => handleSquareClicked(62)}/>
-              <Square value={squares[63]} onSquareClicked={() => handleSquareClicked(63)}/>
+              <Square value={parseCharacter(squares[56])} onSquareClicked={() => handleSquareClicked(56)}/>
+              <Square value={parseCharacter(squares[57])} onSquareClicked={() => handleSquareClicked(57)}/>
+              <Square value={parseCharacter(squares[58])} onSquareClicked={() => handleSquareClicked(58)}/>
+              <Square value={parseCharacter(squares[59])} onSquareClicked={() => handleSquareClicked(59)}/>
+              <Square value={parseCharacter(squares[60])} onSquareClicked={() => handleSquareClicked(60)}/>
+              <Square value={parseCharacter(squares[61])} onSquareClicked={() => handleSquareClicked(61)}/>
+              <Square value={parseCharacter(squares[62])} onSquareClicked={() => handleSquareClicked(62)}/>
+              <Square value={parseCharacter(squares[63])} onSquareClicked={() => handleSquareClicked(63)}/>
           </div>
       </>
   )
